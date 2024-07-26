5617e27f94bd1dec122832b2c808b277f319a604
Ange_Tech(Angel_Molina) <166965796+AngelTech90@users.noreply.github.com>
2024-07-25 22:27:13 -0600

    Initial notes

---
+++ index.ts
@@ -0,0 +1,15 @@
+/*  * For use a ts file we have to compile it before in a jjs file */
+
+/* We jave to init our ts project using: 
+
+"tsc -init" 
+
+*/
+
+/* *For compile our js file we have to use the tsc command in our terminal, for avoid do this again and again we can add tou our command "-w" that meand watch.
+
+This will make that constantly our changes in our ts file be compiled in our ts file:
+
+"tsc script.ts -w"
+
+*/
